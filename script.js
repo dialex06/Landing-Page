590d5971db8bb1e2ed4eeff351d7b994e0f798b9
dialex06 <hoang.2474802010119@vanlanguni.vn>
2026-04-29 14:36:11 +0700

    first commit

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const products = [
   {
     name: "Áo thun",
     price: 100000,
-    img: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
+    img: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=400"
   },
   {
     name: "Quần jeans",
     price: 300000,
-    img: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246"
+    img: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?w=400"
   },
   {
     name: "Giày",
     price: 500000,
-    img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+    img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400"
   }
 ];
